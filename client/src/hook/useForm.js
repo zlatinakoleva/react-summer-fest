42f8ad6a68350f2969b23718341153bf0267cf4a
zlatinakoleva <zlatina.koleva98@gmail.com>
2024-07-27 18:53:02 +0300

    feat(useForm): Add custom hook for forms

---
+++ client/src/hook/useForm.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export function useForm(submitCallback, initialValues) {
+    const [formValues, setFormValues] = useState(initialValues)
+
+    const onChange = (e) => {
+        setFormValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        submitCallback(formValues)
+    } 
+
+    return {
+        formValues,
+        onChange,
+        onSubmit
+    }
+}
